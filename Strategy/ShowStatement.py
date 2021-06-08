c489f529b903869e9c636df0f39eab09eb5495e5
hanseopark <hapark@cern.ch>
2021-06-09 03:24:56 +0900

    Check statement by selected ticker

--- Strategy/ShowStatement.py
+++ Strategy/ShowStatement.py
@@ -23,6 +23,7 @@ for ticker in stock_list:
 
     stats[ticker] = temp
 
+
 combined_stats = pd.concat(stats)
 combined_stats = combined_stats.reset_index()
 
@@ -31,6 +32,18 @@ del combined_stats['level_1']
 combined_stats.columns = ['Ticker', 'Attribute', "Recent"]
 
 # get P/E ratio for each stock
-df = combined_stats[combined_stats.Attribute.str.contains('Trailing P/E')]
+df_per = combined_stats[combined_stats.Attribute.str.contains('Trailing P/E')]
+
+# get P/S ratio for each stock
+df_psr = combined_stats[combined_stats.Attribute.str.contains('Price/Sales')]
+
+# get Price-to-Book ratio for each stock
+df_pbr = combined_stats[combined_stats.Attribute.str.contains("Price/Book")]
+
+# merge dataframe
+df = combined_stats
+
+#df = pd.merge(df_per, df_psr, how='outer', on='Ticker')
+#df = pd.merge(df, df_pbr, how='outer', on='Ticker')
 
 print(df)
