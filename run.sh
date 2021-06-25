65dbb78d05d9345c20cef84b6dbcc485cc4c7c3c
hanseopark <hapark@cern.ch>
2021-06-25 21:43:52 +0900

    fix name

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##echo "sp500" | python BolingerBands/BBEachTicker.py
-python BolingerBands/modelBB.py
+python BollingerBands/modelBB.py
 python RelativeStrengthIndex/modelRSI.py
 echo "selected" | python ChoiceTicker/StocksInfo.py
 echo "selected" | python ChoiceTicker/StocksValue.py
